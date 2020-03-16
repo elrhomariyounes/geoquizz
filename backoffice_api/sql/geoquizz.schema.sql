fb8a7f08c68011869b7c82b0ce452ae6a2cf037a
elrhomari.younes <elrhomari.younes@gmail.com>
2020-03-17 00:52:07 +0100

    Init project : architecture + docker-compose services

---
+++ backoffice_api/sql/geoquizz.schema.sql
@@ -0,0 +1,62 @@
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `mail` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `difficulty`;
+CREATE TABLE `difficulty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(128) NOT NULL,
+  `distance` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `serie`;
+CREATE TABLE `serie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `city` varchar(128) NOT NULL,
+  `map_refs` varchar(128) DEFAULT NULL,
+  `difficulty_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE `photo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(128) NOT NULL,
+  `position` varchar(256) NOT NULL,
+  `url` varchar (256) NOT NULL,
+  `serie_id` int(11) DEFAULT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `game`;
+CREATE TABLE `game` (
+  `id` varchar(128) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `nb_photos` int(11) NOT NULL,
+  `state` int(11) DEFAULT 1,
+  `score` int(11) DEFAULT 0,
+  `player` varchar(128) NOT NULL,
+  `serie_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
