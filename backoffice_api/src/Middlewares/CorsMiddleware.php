014f348a127ccdfe58dd0cc71a5bfde33ba273a3
elrhomari.younes <elrhomari.younes@gmail.com>
2020-03-18 02:36:25 +0100

    api backoffice
    Add CORS

---
+++ backoffice_api/src/Middlewares/CorsMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace gq\backoffice\Middlewares;
+use gq\backoffice\Models\Responses\ErrorResponse;
+use gq\backoffice\Helpers\Response\ResponseWrapper;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+class CorsMiddleware extends AbstractMiddleware
+{
+    public function __invoke(Request $rq, Response $rs, $next)
+    {
+        //No Origin Header
+        if(!$this->OriginIsSet($rq)){
+            $error = new ErrorResponse("error",403,"No Origin Header !!");
+            return ResponseWrapper::errorResponse($error,$rs);
+            return $rs;
+        }
+        $origin = $rq->getHeaderLine('Origin');
+        $rq = $rq->withAttribute('origin',$origin);
+        $response = $next($rq, $rs);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', $origin)
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    }
+
+    private function OriginIsSet(Request $rq){
+        return count($rq->getHeader('Origin'))!=0;
+    }
+}
